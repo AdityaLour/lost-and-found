89a80da97d9a25e2109e591afb914dd81e3ce379
Aditya Lour <louraditya6584@gmail.com>
2026-05-10 01:33:47 +0530

    Add the user schema

---
+++ models/user-schema.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    phoneNumber: {
+      type: String,
+      sparse: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
