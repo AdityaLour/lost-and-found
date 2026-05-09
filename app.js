4bfc051866db9361811c64f846c3c799c661279b
Aditya Lour <louraditya6584@gmail.com>
2026-05-09 19:49:07 +0530

    Add database connection with basic ports

---
+++ app.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const connectDb = require("./db/database");
+const authRoutes = require("./routes/auth-routes");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/auth", authRoutes);
+
+connectDb().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on Port ${process.env.PORT}`);
+  });
+});
