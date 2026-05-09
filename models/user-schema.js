8a9fc11475c88237eceaff546bdee5fb262cb863
Aditya Lour <louraditya6584@gmail.com>
2026-05-10 05:25:09 +0530

    removed the isverified field from user collection

--- models/user-schema.js
+++ models/user-schema.js
@@ -29,10 +29,6 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-    isVerified: {
-      type: Boolean,
-      default: false,
-    },
   },
   {
     timestamps: true,
